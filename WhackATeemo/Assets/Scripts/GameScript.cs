669e54e4f9883799db285feaf7c7cfb8a802f820
James Allen <jax5121@hotmail..com>
2020-02-16 01:15:11 +0000

    Despawning target logic
    Created a project and created logic to spawn a series of targets within a scene and then have the target destroy itself after a certain time

---
+++ WhackATeemo/Assets/Scripts/GameScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public bool spawn;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawn)
+        {        
+            int rand = Random.Range(0, 100);
+            if (rand > 90)
+            {
+                int x = Random.Range(0, 18);
+                int y = Random.Range(0, 10);
+                Instantiate(prefab, new Vector3(x,y,0), Quaternion.identity);
+        
+            }
+        }
+    }
+}
---
+++ WhackATeemo/Assets/Scripts/target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer == 0) // attacks player and disappears
+        {
+            Destroy(this.gameObject);
+        }
+        timer--;
+    }
+}

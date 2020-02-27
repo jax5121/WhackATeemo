1341753d762f135e766a7fb425a01c4d6b36102c
James Allen <jax5121@hotmail.com>
2020-02-27 21:12:56 +0000

    Added health bar and endgame state
    Health bar decreases every second and stops spawning enemies when that happens

---
+++ WhackATeemo/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public static Vector2 new_size;
+    
+    public int game_length;
+
+    private RectTransform rt;
+    private float unit;
+    private int nextUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = this.gameObject.GetComponent<RectTransform>();
+        Debug.Log(rt.rect.width);
+        new_size = new Vector2(rt.rect.width, rt.rect.height);
+        unit = rt.rect.width / game_length;
+        //InvokeRepeating("Update", 0, 1.0f);
+        nextUpdate = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextUpdate)
+        {
+            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+            if (new_size.x <= 0)
+            {
+                new_size.x = 0;
+                TargetControllerScript.spawn = false;
+            }
+            else
+            {
+                new_size.x -= unit;
+            }
+            rt.sizeDelta = new_size;
+        }       
+        
+    }
+}
